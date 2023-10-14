7e5f94e3a59d772d16f1de74380ed60605210130
Deependra Solanki <deepsolanki3010@gmail.com>
2023-10-14 12:42:00 +0530

    Fix console warnings

--- algorithm-visualizer/src/components/darkmode.js
+++ algorithm-visualizer/src/components/darkmode.js
@@ -31,7 +31,7 @@ const DarkMode = () => {
         onChange={toggelTheme}
         defaultChecked={selectedTheme === "dark"}
       />
-      <label className="dark_mode_label" for="darkmode-toggle">
+      <label className="dark_mode_label" htmlFor="darkmode-toggle">
         <Sun />
         <Moon />
       </label>
